e7a218f5f44695371a032281ead74b3bda0f3d34
Chris Song <chris.bk.song@gmail.com>
2020-06-09 13:56:09 -0400

    completes part 2

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react'
 
 import './MemoryCard.css';
-
 import logo from '../images/logo-wrench-white.png';
 
-class MemoryCard extends Component {
+class Card extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFlipped: false,
+    };
+  }
+
+  clickHandler() {
+    this.setState({ isFlipped: !this.state.isFlipped });
+  }
+
   render() {
+    let innerClass = "MemoryCard__inner";
+    if (this.state.isFlipped) {
+      innerClass += ' flipped';
+    }
     return (
-      <div className="MemoryCard">
-        <img src={logo}></img>
+      <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+        <div className={innerClass}>
+          <div className="MemoryCard__back">
+            <img src={logo} alt="" />
+          </div>
+          <div className="MemoryCard__front">
+            ∆
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-export default MemoryCard;
+export default Card;
