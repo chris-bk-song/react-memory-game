6c3f862c197b8d3364615842506312273b981057
Chris Song <chris.bk.song@gmail.com>
2020-06-11 10:15:34 -0400

    Updates

---
+++ precache-manifest.080b5d5484741e9de0ea32b4dfe7fc43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f90c989178849ab8d58d7eb90089ea",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "00015c681e4efdcac524",
+    "url": "/react-memory-game/static/css/main.46fe4bdc.chunk.css"
+  },
+  {
+    "revision": "1fe4847954ad3f467f82",
+    "url": "/react-memory-game/static/js/2.21cb2dde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memory-game/static/js/2.21cb2dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00015c681e4efdcac524",
+    "url": "/react-memory-game/static/js/main.3c25afed.chunk.js"
+  },
+  {
+    "revision": "461bdbb4dd26123f95bf",
+    "url": "/react-memory-game/static/js/runtime-main.64ab7d18.js"
+  },
+  {
+    "revision": "f2d91aa6a55371c5f0c08a6b4e1bd11b",
+    "url": "/react-memory-game/static/media/logo-wrench-white.f2d91aa6.png"
+  }
+]);
